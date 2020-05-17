9c8b9c9052de2e8d4699e11c167d551367732871
michaelrauh <michaelrauh@ymail.com>
2020-05-16 22:10:03 -0400

    successful round trip where a read line with delimiter will print back out the contents of a provided file

--- 1.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-(require 2htdp/batch-io)
-(define (read-split file-name delim)
-  (define contents (read-file file-name))
-  (string-split contents delim))
-
-(module+ test
-  (require rackunit)
-  (check-equal? (read-split "1.txt" #rx", ") '("hello" "here" "are" "words")))
--- expander.rkt
+++ expander.rkt
@@ -2,5 +2,17 @@
 
 (define-macro (aoclop-module-begin (aoclop-program BODY))
   #'(#%module-begin
-     'BODY))
+     BODY))
 (provide (rename-out [aoclop-module-begin #%module-begin]))
+
+(require 2htdp/batch-io)
+(define (read file-number delim)
+  (define contents (read-file (string-append (number->string file-number) ".txt")))
+  (if (equal? "nl" delim)
+      (string-split contents "\n")
+      (string-split contents ", ")))
+(provide read)
+
+(module+ test
+  (require rackunit)
+  (check-equal? (read 1 "nl") '("hello" "here" "are" "words")))
