8caf0537a0fe2066e0c3de833864c0d5f605dc49
michaelrauh <michaelrauh@ymail.com>
2020-05-18 22:24:03 -0400

    WIP take functionality out of expander and make it print out each call with arguments

--- expander.rkt
+++ expander.rkt
@@ -2,22 +2,18 @@
 
 (define-macro (aoclop-module-begin (aoclop-program BODY ...))
   #'(#%module-begin
-     BODY ...
-     (display data)))
+     BODY ...))
 (provide (rename-out [aoclop-module-begin #%module-begin]))
 
 (require 2htdp/batch-io)
 (define (read file-number delim)
-  (define contents (read-file (string-append (number->string file-number) ".txt")))
-  (set! data (string-split contents delim)))
+  `(read @,file-number @,delim))
 (provide read)
 
 (define data empty)
 
 (define (div-each number)
-  (define (divnum x) (/ (string->number x) number))
-  (define ans (map divnum data))
-  (set! data ans))
+  `(div-each @,number))
 
 (define-macro-cases op
   [(op "/" NUMBER) #'(div-each NUMBER)])
