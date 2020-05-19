a5abd4c3d49ae954564a2a2066e47949313f7274
michaelrauh <michaelrauh@ymail.com>
2020-05-18 20:07:59 -0400

    WIP thoughts on scope

--- parser-test.rkt
+++ parser-test.rkt
@@ -11,3 +11,19 @@
 (check-equal?
  (parse-to-datum
   (apply-tokenizer-maker make-tokenizer "\n read: 1 nl | / 3 \n")) '(aoclop-program (read 1 (delimiter "nl")) (op "/" 3)))
+
+; '(aoclop-program (read 1 (delimiter "nl")) (op "/" 3))
+; (read 1 '\n')
+; (div-each 3)
+
+; '(aoclop-program (read 1 (delimiter "nl")) (op "/" 3))
+; (map (λ (x) (/ x 3)) (read 1 '\n'))
+
+; '(aoclop-program (read 1 (delimiter "nl")) (op "/" 3))
+; (for/fold
+; ([res (read 1 '\n')])
+; ([lambdas '((λ (x) (/ x 3))))])
+;  ((lambdas i)))
+
+; '(aoclop-program (read 1 (delimiter "nl")) (op "/" 3))
+; (map (compose1 (λ (x) (/ x 3))) (read 1 '\n'))
