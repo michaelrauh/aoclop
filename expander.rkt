286b7da1d3d266f5642849ba3eb9718f09036c49
michaelrauh <michaelrauh@ymail.com>
2020-05-20 21:04:15 -0400

    remove global state by making the read a child of the op

--- expander.rkt
+++ expander.rkt
@@ -1,20 +1,21 @@
 #lang br/quicklang
 
-(define-macro (aoclop-module-begin (aoclop-program BODY ...))
+(define-macro (aoclop-module-begin (aoclop-program READ OP))
+  (with-pattern ([(read NUM DELIM-EXPR) #'READ]
+                 [(op FUNC ARG) #'OP])
+    
   #'(#%module-begin
-     BODY ...))
+     (op FUNC ARG (read NUM DELIM-EXPR)))))  ; this line shows that the with pattern broke things down too much
 (provide (rename-out [aoclop-module-begin #%module-begin]))
 
 (require 2htdp/batch-io)
 (define (read file-number delim)
   (define contents (read-file (string-append (number->string file-number) ".txt")))
-  (set! data (string-split contents delim)))
+  (string-split contents delim))
 (provide read)
 
-(define data empty)
-
 (define-macro-cases op
-  [(op "/" NUMBER) #'(map (λ (x) (/ (string->number x) NUMBER)) data)])
+  [(op "/" NUMBER READ-EXPR) #'(map (λ (x) (/ (string->number x) NUMBER)) READ-EXPR)])
 (provide op)
 
 (define-macro-cases delimiter
