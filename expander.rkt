c8f838b7e8a91fc129361f9b0ea108883d98240f
michaelrauh <michaelrauh@ymail.com>
2020-05-21 20:38:37 -0400

    add case for divide then floor, but it does not generalize yet

--- expander.rkt
+++ expander.rkt
@@ -1,8 +1,8 @@
 #lang br/quicklang
 
-(define-macro (aoclop-module-begin (aoclop-program READ (op FUNC ...)))
+(define-macro (aoclop-module-begin (aoclop-program READ (op FUNC ...) OPS ...))
   #'(#%module-begin
-     (op FUNC ... READ)))
+     (op FUNC ... READ OPS ...)))
 (provide (rename-out [aoclop-module-begin #%module-begin]))
 
 (require 2htdp/batch-io)
@@ -13,7 +13,8 @@
 
 (define-macro-cases op
   [(op "/" NUMBER READ-EXPR) #'(map (λ (x) (/ (string->number x) NUMBER)) READ-EXPR)]
-  [(op "floor" READ-EXPR) #'(map (λ (x) (floor (string->number x))) READ-EXPR)])
+  [(op "floor" READ-EXPR) #'(map (λ (x) (floor (string->number x))) READ-EXPR)]
+  [(op "/" NUMBER READ-EXPR (op "floor")) #'(map (λ (x) (floor (/ (string->number x) NUMBER))) READ-EXPR)])
 (provide op)
 
 (define-macro-cases delimiter
