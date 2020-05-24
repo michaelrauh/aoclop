ac9cca41669313775172d905aab421aa15d9753f
michaelrauh <michaelrauh@ymail.com>
2020-05-24 15:47:36 -0400

    rearrange in expander to put the read inside of the scope block

--- expander.rkt
+++ expander.rkt
@@ -1,20 +1,6 @@
 #lang br/quicklang
 
-(define-macro (aoclop-module-begin (aoclop-program READ (op FUNC ARG)))
+(define-macro (aoclop-module-begin (aoclop-program READ (scope-block OP)))
   #'(#%module-begin
-     (op FUNC ARG READ)))
+     '(scope-block OP READ)))
 (provide (rename-out [aoclop-module-begin #%module-begin]))
-
-(require 2htdp/batch-io)
-(define (read file-number delim)
-  (define contents (read-file (string-append (number->string file-number) ".txt")))
-  (string-split contents delim))
-(provide read)
-
-(define-macro-cases op
-  [(op "/" NUMBER READ-EXPR) #'(map (λ (x) (/ (string->number x) NUMBER)) READ-EXPR)])
-(provide op)
-
-(define-macro-cases delimiter
-  [(delimiter "nl") #'"\n"])
-(provide delimiter)
