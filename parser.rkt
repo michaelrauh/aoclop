5b456e8ba54edc463e2986accec8cb66d904f6d0
michaelrauh <michaelrauh@ymail.com>
2020-12-15 17:28:40 -0500

    working solution for 1.1

--- parser.rkt
+++ parser.rkt
@@ -1,7 +1,8 @@
 #lang brag
-aoclop-program : /NEWLINE read scope-block /NEWLINE
+aoclop-program : /NEWLINE read scope-block /PIPE collect /NEWLINE
 read           : /READ INTEGER delimiter
 scope-block    : /PIPE /DOWNSCOPE /PIPE all-ops /PIPE /UPSCOPE
 delimiter      : DELIMITER
 all-ops        : op*
-op             : OP | OP /PIPE | OP INTEGER | OP INTEGER /PIPE
+op             : OP | OP INTEGER | OP /PIPE | OP INTEGER /PIPE
+collect        : COLLECT
