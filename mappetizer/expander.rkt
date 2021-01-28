32446be575e908eb484f5a76c6d632ecb107384b
michaelrauh <michaelrauh@ymail.com>
2021-01-27 20:36:17 -0500

    use syntax parse for mappetizer

--- mappetizer/expander.rkt
+++ mappetizer/expander.rkt
@@ -6,6 +6,8 @@
 (provide read)
 (provide delimiter)
 
+(require(for-syntax syntax/parse))
+
 (define (converge proc x)
   (define step (proc x))
   (cond [(<= step 0) 0]
@@ -16,20 +18,22 @@
 (provide mappetizer-program)
 
 (define-syntax (read-scope stx)
-  (syntax-case stx ()
-    [(read-scope (scope-block (converge-block all-ops)) read) #'(map ((curry converge) all-ops) read)]
-    [(read-scope (scope-block all-ops) read) #'(map all-ops read)]))
+  (syntax-parse stx
+    #:datum-literals (read-scope scope-block converge-block)
+    [(read-scope (scope-block (converge-block all-ops:expr)) read:expr) #'(map ((curry converge) all-ops) read)]
+    [(read-scope (scope-block all-ops:expr) read:expr) #'(map all-ops read)]))
 
 (define-syntax-rule (all-ops op ...)
   (λ~> op ...))
 (provide all-ops)
 
 (define-syntax (op stx)
-  (syntax-case stx ()
+  (syntax-parse stx
+    #:datum-literals (op)
     [(op x "identity") #'(identity x)]
     [(op x "floor") #'(floor x)]
-    [(op x "/" divisor) #'(/ x divisor)]
-    [(op x "-" difference) #'(- x difference)]))
+    [(op x "/" divisor:number) #'(/ x divisor)]
+    [(op x "-" difference:number) #'(- x difference)]))
 (provide op)
 
 (define-syntax-rule (collect "sum") +)
