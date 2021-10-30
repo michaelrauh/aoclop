55bd644c5246142ad424ebac7d1d678fac51bbdc
michaelrauh <michaelrauh@ymail.com>
2021-10-30 19:56:34 -0400

    introduce step distance, but serious performance regression

--- graphical/graph.rkt
+++ graphical/graph.rkt
@@ -1,26 +1,31 @@
 #lang racket
 
-(struct point (x y) #:transparent)
+(struct point (x y))
+(struct traversal (point step))
 (provide graph%)
 
 (define (find-y-path p y)
-  (define start-y (point-y p))
-  (define fill-x (point-x p))
+  (define start-y (point-y (traversal-point p)))
+  (define fill-x (point-x (traversal-point p)))
+  (define start-step (traversal-step p))
   (define sign (if (positive? y) 1 -1))
   (define ys (range (+ sign start-y) (+ start-y y sign) sign))
-  (map (λ (y-pos) (point fill-x y-pos)) ys))
+  (define steps (range start-step (+ start-step (length ys))))
+  (map (λ (y-pos step) (traversal (point fill-x y-pos) step)) ys steps))
 
 (define (find-x-path p x)
-  (define start-x (point-x p))
-  (define fill-y (point-y p))
+  (define start-x (point-x (traversal-point p)))
+  (define start-step (traversal-step p))
+  (define fill-y (point-y (traversal-point p)))
   (define sign (if (positive? x) 1 -1))
   (define xs (range (+ sign start-x) (+ start-x x sign) sign))
-  (map (λ (x-pos) (point x-pos fill-y)) xs))
+  (define steps (range start-step (+ start-step (length xs)))) ; steps may be off by one
+  (map (λ (x-pos step) (traversal (point x-pos fill-y) step)) xs steps))
 
 (define graph%
   (class object%
     (super-new)
-    (define origin (point 0 0))
+    (define origin (traversal (point 0 0) 0))
     (define current-color 0)
     (define positions (make-hash (list (cons current-color (list origin)))))
 
@@ -32,9 +37,26 @@
       (define current-color-positions (hash-ref positions current-color))
       
       (define old-position (car current-color-positions))
-      (define path (if (not (zero? x)) (find-x-path old-position x) (find-y-path old-position y))) ; note: this assumes movement across one axis at a time, which is guaranteed by the question for now
+      (define path (if (not (zero? x)) (find-x-path old-position x) (find-y-path old-position y)))
       (define result-path (append (reverse path) current-color-positions))
       (hash-set! positions current-color result-path))
+
+    (define (find-intersections l) ; prefer intersections with lower step
+      (define against (car l))
+      (define others (cdr l))
+      (for/list ([x against]
+             #:when (in-all x others))
+    x))
+
+    (define (in-all x yss)
+      (for/and ([ys yss])
+        (member-of-list x ys)))
+
+    (define (member-of-list x ys)
+      (member x ys pointmember))
+
+    (define (pointmember x y)
+      (and (= (point-x (traversal-point x)) (point-x (traversal-point y))) (= (point-y (traversal-point x)) (point-y (traversal-point y)))))
 
     (define/public (intersects)
       (define (filter-list-origin l)
@@ -44,7 +66,7 @@
                                          (map filter-list-origin
                                               (map (λ (x) (hash-ref positions x))
                                                    (range (add1 current-color))))))
-      (define intersections (set->list (apply set-intersect (map list->set nonempty-possibles)))) ; note: this optimization is only possible because order does not matter, which is guaranteed by the question for now
+      (define intersections (find-intersections nonempty-possibles)) 
       (new intersects% [intersections intersections]))))
 
 (define intersects%
@@ -55,8 +77,8 @@
     
     (define/public (magnitudes)
       (new magnitudes% [magnitudes (map (λ (p)
-                                          (+ (abs (point-x p))
-                                             (abs (point-y p))))
+                                          (+ (abs (point-x (traversal-point p)))
+                                             (abs (point-y (traversal-point p)))))
                                         ints)]))))
 
 (define delays%
@@ -83,6 +105,5 @@
   (send graph changecolor)
   (send graph add 0 7)
   (define res (send (send (send graph intersects) magnitudes) minimum))
-
   (check-equal? res 5))
 
